3bfaf50fa6a6029a69391f76a452817d379a3ed3
tancredegorand <119679491+tancredegorand@users.noreply.github.com>
2024-01-29 16:47:16 +0100

    dogcard

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
-import Button from "./components/Button.vue"
-import Article from "./components/Article.vue"
+import Button from "./components/Button.vue" 
+import DogCard from "./components/DogCard.vue"
 
 <template>
-Hello
-</template>
-
+    <div class="dogs-gallery">
+      <DogCard firstname="Pluto" breed="Saint-Hubert" picture_url="https://my-best-kennel.com/pictures/45.png"/>
+      <DogCard firstname="Rex" breed="Labrador" picture_url="https://my-best-kennel.com/pictures/46.png"/>
+    </div>
+  </template>
 <script>
-export default {}
-</script> 
-
+export default {};
+</script>
---
+++ src/components/DogCard.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <div class="dog-card">
+      <img class="picture" src="https://my-best-kennel.com/pictures/45.png"/>
+      <div>
+        <h2 class="firstname">Pluto</h2>
+        <p class="breed">Saint-Hubert</p>
+      </div>  
+    </div>
+    
+  </template>
+  
+  <script>
+      export default {
+        name: 'DogCard'
+      }
+  </script>
+  
+  <style scoped>
+      .dog-card {
+        display: flex;
+      }
+  
+      h2 {
+           font-size: 20px;
+      }
+  </style>
