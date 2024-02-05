7019621497d225832d6947ce960e7f8c62cf3f46
tancredegorand <119679491+tancredegorand@users.noreply.github.com>
2024-02-05 14:22:30 +0100

    openjson?

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 
 
 <template>
-  <div class="dogs-gallery">
     <DogCard firstname="Pluto" breed="Saint-Hubert" picture_url="https://my-best-kennel.com/pictures/45.png"/>
-    <DogCard :firstname = "TrendingCreatorsData.user_list[0].user.nickname" breed="Labrador" picture_url="https://my-best-kennel.com/pictures/46.png"/>
-  </div>
+    <DogCard firstname="Pluto" breed="Saint-Hubert" picture_url="https://my-best-kennel.com/pictures/45.png"/>
+    <DogCard firstname="Pluto" breed="Saint-Hubert" picture_url="https://my-best-kennel.com/pictures/45.png"/>
+
 </template>
 
 <script>
+//import Files
+import TrendingCreatorsData  from "./services/api/trending-creator.json"
+
+//import Components
 import DogCard from "./components/DogCard.vue"
 
-import {getTrendingCreators} from './services/api/getTrendingCreators.js'
+//import functions
+import {getTrendingCreatorsFromFile} from "./services/api/getTrendingCreators"
+
 
 export default {
   name: "App",
-
   components:{
     DogCard,
   },
-  
+
   data() {
     return {
       TrendingCreatorsData: []
+    }, {
+        TrendingCreators: TrendingCreatorsData  
     }
   },
-
 
   created: function() {
     this.retrieveTrendingCreators()
@@ -32,9 +38,11 @@ export default {
 
   methods: {
     async retrieveTrendingCreators() {
-      //this.TrendingCreatorsData = await getTrendingCreators("US"); 
+
     }
   }
 
 };
+
+
 </script>
